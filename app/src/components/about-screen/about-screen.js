9ee5fbcee2a7812a221f7a2dcd0162c7518f7699
Trevor Howard <trevorhoward2@hotmail.com>
2021-08-14 18:26:59 -0600

    Fix mobile ui sizing bugs, consolidate test files

--- app/src/components/about-screen/about-screen.js
+++ app/src/components/about-screen/about-screen.js
@@ -14,8 +14,8 @@ const AboutScreen = ({pageRefs}) => {
                     <img src={profilePic} alt="trevor howard"/>
                 </div>
                 <div className="elevator-pitch">
-                    My name is Trevor Howard and I've been focused over the last 2 years developing
-                    software since September of '18. After my first full time job in the industry (a start up), I've
+                    My name is Trevor Howard and I've been focused over the last few years developing
+                    software since September of 2018. After my first full time job in the industry (a start up), I've
                     spent the last 2 years at a relatively small healthcare focused company, creating and maintaining a wide range
                     of applications. I am most interested in working for company that priortizes . I'm looking for
                     opportunities that are focused on providing elegant solutions to everyday problems.
