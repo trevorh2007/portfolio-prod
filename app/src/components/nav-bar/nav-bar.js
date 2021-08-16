eb02d63074b0435686df36cdf55d2dee51a3502f
Trevor Howard <trevorhoward2@hotmail.com>
2021-08-15 21:50:35 -0600

    add resume link to nav bar

--- app/src/components/nav-bar/nav-bar.js
+++ app/src/components/nav-bar/nav-bar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './nav-bar.scss'
 
+import resumePDF from '../../assets/pdf/Trevor Howard - Resume.pdf'
+
 function NavBar({ pageRefs }){
     function scrollIntoView(type) {
         pageRefs.current[type].scrollIntoView({ behavior: "smooth" });
@@ -17,7 +19,8 @@ function NavBar({ pageRefs }){
                     <div className="page-link" onClick={() => scrollIntoView('home')}>Home</div>  
                     <div className="page-link" onClick={() => scrollIntoView('about')}>About</div>
                     <div className="page-link" onClick={() => scrollIntoView('projects')}>Projects</div>
-                    <div className="page-link" onClick={() => scrollIntoView('contact')}>Contact</div>  
+                    <div className="page-link" onClick={() => scrollIntoView('contact')}>Contact</div>
+                    <div className="page-link" onClick={() => window.open(resumePDF, "_blank")}>Resume</div>
                 </div>  
             </nav>
         </div>
