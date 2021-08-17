439da0e30135914a462c4a618986c65bf8f3cfda
Trevor Howard <trevorhoward2@hotmail.com>
2021-08-16 21:05:48 -0600

    Complete footer content, icons, backtotop btn

--- app/src/components/footer/footer.js
+++ app/src/components/footer/footer.js
@@ -1,16 +1,23 @@
 import './footer.scss'
+import {RiLinkedinBoxLine, RiGithubFill, RiCopyrightLine} from 'react-icons/ri'
+import {HiChevronDoubleUp} from 'react-icons/hi'
 
 const Footer = () => {
-
     return (
         <section id="footer" className="footer-section">
+            <div className="chevron-up" onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}>
+                <HiChevronDoubleUp />
+            </div>
             <div className="icon-wrap flex-row">
                 <a href="https://www.linkedin.com/in/trevor-howard-ut/">
-                    linked in
+                    <RiLinkedinBoxLine />
                 </a>
                 <a href="https://github.com/trevorh2007">
-                    github
+                    <RiGithubFill />
                 </a>
+            </div>
+            <div className="copyright">
+                TREVOR HOWARD&nbsp;<RiCopyrightLine />{new Date().getFullYear()}
             </div>
         </section>
     )
