ac3375e4feba018c2fd33777fe07bfc20c94baf2
Trevor Howard <trevorhoward2@hotmail.com>
2021-08-24 07:21:37 -0600

    basic styling and responsiveness to project cards

--- app/src/components/projects-screen/projects-screen.js
+++ app/src/components/projects-screen/projects-screen.js
@@ -1,10 +1,13 @@
-import React, { useState, useRef } from 'react'
+import React, { useRef } from 'react'
 import './projects-screen.scss'
 
 import ProjectModal from '../project-modal/project-modal'
 
 const ProjectsScreen = ({pageRefs}) => {
   const modal = useRef(null)
+  const modal2 = useRef(null)
+  const modal3 = useRef(null)
+  const modal4 = useRef(null)
 
   return (
     <section id="projects" className="projects-section" ref={el => pageRefs.current = { ...pageRefs.current, projects: el }}>
@@ -14,10 +17,30 @@ const ProjectsScreen = ({pageRefs}) => {
           <div className="section-header-bar" />
         </div>
         <div className="project-gallery">
-          <div className="cards">
-            <button onClick={() => modal.current.open()}>Show modal</button>
-            <ProjectModal ref={modal}>
-              Quick test, card will go here later
+          <div className="project-cards">
+            <div className="card" onClick={() => modal.current.open()} >
+              Project Image 1
+            </div>
+            <ProjectModal ref={modal} true>
+              Project Modal 1
+            </ProjectModal>
+            <div className="card" onClick={() => modal2.current.open()}>
+              Project Image 2
+            </div>
+            <ProjectModal ref={modal2} fade>
+              Project Modal 2
+            </ProjectModal>
+            <div className="card" onClick={() => modal3.current.open()}>
+              Project Image 3
+            </div>
+            <ProjectModal ref={modal3} true>
+              Project Modal 3
+            </ProjectModal>
+            <div className="card" onClick={() => modal4.current.open()}>
+              Project Image 4
+            </div>
+            <ProjectModal ref={modal4} fade>
+              Project Modal 4
             </ProjectModal>
           </div>
         </div>
