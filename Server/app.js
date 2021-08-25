63d6bd9944f2eb08f83dfaf4df2f9899a4578feb
Trevor Howard <trevorhoward2@hotmail.com>
2021-08-25 12:23:40 -0600

    clean up packages and unnecessary files

--- Server/app.js
+++ Server/app.js
@@ -1,31 +1,23 @@
 require('dotenv').config()
-var createError = require('http-errors')
 var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
-var logger = require('morgan')
 const cors = require('cors')
 
 var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
 
 var app = express()
 
+const PORT = process.env.PORT || 5000;
 const mailer = require('./email')
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
-
 app.use(cors())
-app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
 
 // error handler
 app.use(function(err, req, res, next) {
