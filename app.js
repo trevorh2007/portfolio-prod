0d6dbf855f873f195771630529256b1995984d19
Trevor Howard <trevorhoward2@hotmail.com>
2021-08-25 13:42:38 -0600

    re-organize for production

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 var express = require('express')
 var path = require('path')
-var cookieParser = require('cookie-parser')
 const cors = require('cors')
 
 var indexRouter = require('./routes/index')
@@ -14,7 +13,6 @@ const mailer = require('./email')
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.use('/', indexRouter)
